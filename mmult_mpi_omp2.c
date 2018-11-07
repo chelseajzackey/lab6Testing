21227f79f1064acb7fdf3f8890aaba1694778947
youngchuleta <43212850+youngchuleta@users.noreply.github.com>
2018-11-07 00:31:23 -0500

    Update mmult_mpi_omp2.c

--- mmult_mpi_omp2.c
+++ mmult_mpi_omp2.c
@@ -75,7 +75,7 @@ int main(int argc, char* argv[])
             endtime = MPI_Wtime();
             printf("%f\n",(endtime - starttime));
             cc2  = malloc(sizeof(double) * a_nrows * b_ncols);
-            mmult2(cc2, aa, a_nrows, a_ncols, bb, b_ncols, b_nrows);
+            mmult(cc2, aa, a_nrows, a_ncols, bb, b_ncols, b_nrows);
             compare_matrices(cc2, cc1, a_nrows, b_ncols);
             }
         } else {
